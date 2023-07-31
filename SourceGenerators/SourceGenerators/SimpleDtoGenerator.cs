b2287fa59851dc9c6839961fa8779d03bde4cca2
Yassine FERNANE <yfernane@outlook.fr>
2023-07-31 22:41:00 +0200

    feat: create SimpleDtoGenerator.cs

---
+++ SourceGenerators/SourceGenerators/SimpleDtoGenerator.cs
@@ -0,0 +1,17 @@
+using Microsoft.CodeAnalysis;
+
+namespace SourceGenerators;
+
+[Generator]
+public class SimpleDtoGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+}
